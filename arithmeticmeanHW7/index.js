c392422cca40089f0775f2a3b298fad8f43d9392
vladpetrenko02 <johnnyproduction023@gmail.com>
2023-02-09 04:13:45 +0200

    Added HW7

---
+++ arithmeticmeanHW7/index.js
@@ -0,0 +1,6 @@
+const frstNum = Number(prompt("Enter first number"));
+const scndNum = Number(prompt("Enter second number"));
+const thrdNum = Number(prompt("Enter third number"));
+
+alert(`Arithmetic mean is ${(frstNum + scndNum + thrdNum) / 3}`);
+
