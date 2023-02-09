b9e2f6a6c021a3037e07acd5171a58d748a2699b
vladpetrenko02 <johnnyproduction023@gmail.com>
2023-02-09 03:37:44 +0200

    Added HW4

---
+++ mathoperandHW4/index.js
@@ -0,0 +1,4 @@
+const frstNum = Number(prompt("First number: "));
+const scndNum = Number(prompt("Second number: "));
+alert(`${frstNum} + ${scndNum} = ${frstNum + scndNum}\n${frstNum} - ${scndNum} = ${frstNum - scndNum}
+${frstNum} * ${scndNum} = ${frstNum * scndNum}\n${frstNum} / ${scndNum} = ${frstNum / scndNum}`);
