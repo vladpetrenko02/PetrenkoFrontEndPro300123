081153e5d92fbca530ed6aeca237f34216c261ee
vladpetrenko02 <johnnyproduction023@gmail.com>
2023-02-09 03:12:58 +0200

    Added HW3

---
+++ strnumHW3/index.js
@@ -0,0 +1,8 @@
+// Task One
+const frstStr = prompt("Enter first string");
+const scndStr = prompt("Enter second string");
+const thrdStr = prompt("Enter third string");
+alert(`First string: ${frstStr}\nSecond string: ${scndStr}\nThird string: ${thrdStr}\n`);
+
+// Task Two
+alert(`Your answer is ${prompt("Enter number").split("").join(" ")}`);
